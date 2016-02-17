e2e03201dfa9e356f6e66a9cf06702ae35e6c9e7
Chang <peichanghong2@gmail.com>
2016-02-17 09:29:17 +0800

    Complete and pass Unit Testing for Sort function

--- Unit Testing/Unit Testing.cpp
+++ Unit Testing/Unit Testing.cpp
@@ -21,8 +21,29 @@ namespace DataFileTest
 
 			actualString = DataFileTest._dataFile[0];
 			Assert::AreEqual(expectedString, actualString );
-		}
+			}
 		}
 
+		TEST_METHOD(SortingTest)
+		{
+			DataFile DataFileTest;
+			std::string actualString;
+			std::string expectedString[4] = {"abba","bob","cat","rabbit"};
+			int i = 0;
+
+			DataFileTest.displayWelcomePage();
+			DataFileTest.addLineToDataFile("cat");
+			DataFileTest.addLineToDataFile("rabbit");
+			DataFileTest.addLineToDataFile("bob");
+			DataFileTest.addLineToDataFile("abba");
+
+			DataFileTest.sortDataFileAlphabetically();
+
+			while(i!=4) {
+				actualString = DataFileTest._dataFile[i];
+				Assert::AreEqual(expectedString[i], actualString );
+				i++;
+			}
+		}
 	};
 }
